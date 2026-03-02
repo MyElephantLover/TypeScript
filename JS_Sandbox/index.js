1af4c1b078adf1bc8c90240faa9a73f2c97718c4
MyElephantLover <jin.summer.1993@gmail.com>
2026-03-02 16:21:23 -0500

    added

--- JS_Sandbox/index.js
+++ JS_Sandbox/index.js
@@ -182,9 +182,17 @@ let result = value ?? 'Default Value'; // if the value is faulsy, it does not re
 console.log(user.address?.city?? 'Unknown');
 
 const notes = ['Meeting Note', 'Grocery List'];
-const newNotes = [...notes, 'Workout Plan'];
-console.log(notes);
+// const newNotes = [...notes, 'Workout Plan'];
+// console.log(notes);
+// console.log(newNotes);
+
+const newNotes = notes.map((note) => 
+    note === 'Grocery List' ? 'Shopping List' : note
+);
+
 console.log(newNotes);
+console.log(notes);
+
 
 
 
